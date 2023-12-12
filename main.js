cc4ea209e4a42e2ca24ba89c95fe932d4d610f25
nimbus <christophercamorrison@gmail.com>
2023-12-12 18:08:41 -0500

    Pushing today's work

--- main.js
+++ main.js
@@ -1,12 +1,20 @@
 const path = require ('path');
 const {app, BrowserWindow} = require ('electron');
 
+const isDev = process.env.NODE_ENV !== 'development';
+const isMac = process.platform === 'dawrin';
+
 function createMainWindow() {
     const mainWindow = new BrowserWindow({
         title: "Habix",
-        width: 500,
-        height: 600
+        width: isDev? 1000 : 500,
+        height: 500,
     })
+
+//Open Devtools if in dev env
+if (isDev){
+    mainWindow.webContents.openDevTools();
+}
 
     mainWindow.loadFile(path.join(__dirname, './renderer/index.html'));
 
@@ -14,4 +22,17 @@ function createMainWindow() {
 
 app.whenReady().then(() => {
     createMainWindow();
+
+    app.on('activate', () => {
+        if (BrowserWindow.getAllWindows().length === 0){
+            createMainWindow();
+        }
+    })
+
+})
+
+app.on('window-all-closed', () => {
+    if (!isMac) {
+        app.quit()
+    }
 })
