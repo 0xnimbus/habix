a36739a5094b2b54b9ac5ae38fb030a9f475d6da
nimbus <christophercamorrison@gmail.com>
2023-12-18 19:08:28 -0500

    FINALLY GOT ELECTRON TO RENDER REACT WITH JAVASCRIPT

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 const path = require ('path');
 const {app, BrowserWindow} = require ('electron');
+const url = require('url')
 
 const isDev = process.env.NODE_ENV !== 'development';
 const isMac = process.platform === 'dawrin';
@@ -9,30 +10,30 @@ function createMainWindow() {
         title: "Habix",
         width: isDev? 1600 : 1600,
         height: 900,
+        webPreferences: {
+            nodeIntegration: true,
+            // contextIsolation: true, // Disable context isolation
+            // enableRemoteModule: false, // Enable remote module (if needed)
+            // webSecurity: true, // Disable web security (for debugging purposes)
+        },
     })
+
+    const startUrl = url.format({
+    
+        pathname: path.join(__dirname, './app/build/index.html'),
+        protocol: 'file', 
+    }) 
+
+    mainWindow.loadURL(startUrl)
 
 //Open Devtools if in dev env
-if (isDev){
-    mainWindow.webContents.openDevTools();
-}
-
-    mainWindow.loadFile(path.join(__dirname, './my-app/public/index.html'));
-
-}
-
-app.whenReady().then(() => {
-    createMainWindow();
-
-    app.on('activate', () => {
-        if (BrowserWindow.getAllWindows().length === 0){
-            createMainWindow();
-        }
-    })
-
-})
-
-app.on('window-all-closed', () => {
-    if (!isMac) {
-        app.quit()
+    if (isDev){
+        mainWindow.webContents.openDevTools();
     }
-})
+
+}
+
+app.whenReady().then(createMainWindow)
+
+
+
